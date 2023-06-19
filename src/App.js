a49c35594cd0c5f407ba09f0b788d704229cee98
AryoDjat1 <aryodjati5@gmail.com>
2023-06-19 09:21:50 +0700

    fix Header and App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React, { useEffect, useState } from 'react'
-import Header from './components/Header'
-import TodoInput from './components/TodoInput'
-import TodoItem from './components/TodoItem'
+import { useState, useEffect } from 'react';
+import Header from './components/Header';
+import TodoInput from './components/TodoInput';
+import TodoItem from './components/TodoItem';
 
 const App = () => {
   const [todoList, setTodoList] = useState([]);
-  const [newTodoName, setNewTodoName] = useState([]);
-  const [todoId, setTodoId] = useState([]);
-  
+  const [newTodoName, setNewTodoName] = useState('');
+  const [todoId, setTodoId] = useState(0);
+
   useEffect(() => {
     readLocalStorage();
   }, []);
 
-  useEffect(()=> {
+  useEffect(() => {
     writeLocalStorage();
   }, [todoList]);
 
@@ -20,40 +20,44 @@ const App = () => {
     const data = localStorage.getItem('todoList');
     if (data) {
       setTodoList(JSON.parse(data));
-      setTodoList(JSON.parse(data).length);
+      setTodoId(JSON.parse(data).length);
     }
-  }
+  };
 
-  const writeLocalStorage = () =>{
+  const writeLocalStorage = () => {
     localStorage.setItem('todoList', JSON.stringify(todoList));
-  }
+  };
 
-  const addTodo = () =>{
-    setTodoList([...todoList, {
-      id: todoId,
-      name: newTodoName,
-      done: false
-    },]);
-        setNewTodoName('');
-        setTodoId((prevId)=>prevId+1);
-  }
+  const addTodo = () => {
+    setTodoList([
+      ...todoList,
+      {
+        id: todoId,
+        name: newTodoName || 'New Todo',
+        done: false,
+      },
+    ]);
+    setNewTodoName('');
+    setTodoId((prevId) => prevId + 1);
+  };
+
+  const removeTodo = (id) => {
+    setTodoList(todoList.filter((todo) => todo.id !== id));
+  };
 
   const toggleTodo = (id) => {
     setTodoList((prevTodoList) =>
-    prevTodoList.filter((todo) => todo.id!== id).map((todo)=>{
-      if (todo.id === id) {
-        return {
-          ...todo,
-          done: !todo.done,
-        };
-      }
-      return todo;
-    }))
-  }
-
-  const removeTodo = (id) => {
-    setTodoList(todoList.filter((todo) =>todo.id !== id));
-  }
+      prevTodoList.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            done: !todo.done,
+          };
+        }
+        return todo;
+      })
+    );
+  };
 
   const completedTodoNum = todoList.filter((todo) => todo.done).length;
   const totalTodos = todoList.length;
@@ -79,7 +83,7 @@ const App = () => {
         </div>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
