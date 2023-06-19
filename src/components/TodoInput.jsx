cf70d5779d8a8ac2dda40ca891ef806ef7a92cc5
AryoDjat1 <aryodjati5@gmail.com>
2023-06-19 09:22:38 +0700

    fixing Todo Iput

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
 
-const TodoInput = ({ newTodoName, handleNewTodoNameChange, handelAddTodo}) => {
-  return (
-    <div id='todoInput'>
-        <div className='listItem'>
-            <input
-                type="text"
-                value={{ newTodoName }}
-                onChange={handleNewTodoNameChange}
-                placeholder='Input here and return to add...'
-                onKeyDown={(e) => {
-                     if (e.key === 'Enter') {
-                        handelAddTodo()                        
-                    }
-                }} />
+const TodoInput = ({ newTodoName, handleNewTodoNameChange, handleAddTodo }) => {
+    return (
+      <div id="todoInput">
+        <div className="listItem">
+          <input
+            type="text"
+            value={newTodoName}
+            onChange={handleNewTodoNameChange}
+            placeholder="Input here and return to add..."
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                handleAddTodo();
+              }
+            }}
+          />
         </div>
-    </div>
-  )
-}
+      </div>
+    );
+  };
 
 export default TodoInput
